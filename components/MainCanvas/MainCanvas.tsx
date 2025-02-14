6dc6a24574c2ed57ab8e250fa5a9fc359c18c804
mauricio-ruizruiz <mauricio.ruizruiz85@gmail.com>
2025-02-13 19:35:53 -0600

    Downgrade to NextJs 14.0.1 to work with Text from Drei

--- components/MainCanvas/MainCanvas.tsx
+++ components/MainCanvas/MainCanvas.tsx
@@ -10,60 +10,25 @@ function MainCanvas({ appContainerRef, homeTop, scrollHome }: any) {
   const created = () => {
     console.log('created');
   };
-
-  // Anta FontLoader
-  // const antaFont = useFont('./fonts/Anta_Regular.json');
-  // useFont.preload('./fonts/Anta_Regular.json');
+  const antaFont = useFont('./fonts/Anta_Regular.json');
   const variants = {
     hidden: { x: 0 },
     visible: { x: 1 },
   };
-  // Scroll Progress
-  // const { scrollYProgress } = useScroll({
-  //   container: appContainerRef,
-  //   target: homeTop,
-  //   offset: ['start', 'start end'],
-  //   layoutEffect: false,
-  // });
   const x = useTransform(scrollHome, [0, 1], [0, Math.PI * -0.5]);
   return (
     <>
       <section className={classes.canvas} ref={canvasContainerRef}>
-        <Canvas
-          onCreated={created}
-          // eventSource={canvasContainerRef.current}
-          // eventPrefix="client"
-          camera={{ fov: 32, position: [0, 0, 20] }}
-          flat
-          // children={undefined}
-        >
-          {/* <OrbitControls /> */}
-          <motion.group position={[0, 1, -0.5]} rotateX={x} anchorX={'center'}>
-            <Text
-              fontSize={0.35}
-              textAlign="center"
-              anchorX={'center'}
-              // font="./fonts/anta-v1-latin-regular.woff2"
-              // font={'./fonts/Anta_Regular.json'}
-            >
+        <Canvas onCreated={created} camera={{ fov: 32, position: [0, 0, 20] }} flat>
+          <motion.group position={[0, 1, -0.5]} rotation={[x, 0, 0]} anchorX={'center'}>
+            <Text fontSize={0.35} textAlign="center" anchorX={'center'} font={antaFont}>
               mauricio ruiz ruiz
             </Text>
           </motion.group>
 
           <ambientLight />
-          {/* <Html position={[0, 1, 0]} center={true}>
-            <h1 style={{ width: 'max-content', fontSize: '2.5rem' }}>mauricio ruiz ruiz</h1>
-          </Html> */}
 
-          <motion.group
-            position={[0, 0, 0]}
-            scale={[1, 1, 1]}
-            rotateX={x}
-            // rotation-x={x}
-            // initial="hidden"
-            // animate="visible"
-            // variants={variants}
-          >
+          <motion.group position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[x, 0, 0]}>
             <mesh>
               <boxGeometry args={[3, 1, 1]} />
             </mesh>
